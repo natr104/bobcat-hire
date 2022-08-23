ee9944d727b550d2ffc4a455601d71d03c1533c2
natr104 <troy.nathan@gmail.com>
2022-08-23 22:41:27 +1000

    fixed retrieval of current user

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,17 +4,18 @@ import { useSelector } from "react-redux"
 export default function Profile() {
     
     const { user: currentUser } = useSelector((state) => state.auth);
+    console.log(currentUser)
     if (!currentUser) {
         return <Navigate to='/login' />
     }
-    const user = currentUser.user
+
 
     return (
         <>
         <h3>
-            {user.name}'s Profile
+            {currentUser.name}'s Profile
         </h3>
-        <p>{user.email}</p>
+        <p>{currentUser.email}</p>
         
         </>
     )

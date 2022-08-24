abf5657b7ba42bc2c91669d297879dafefd753dd
natr104 <troy.nathan@gmail.com>
2022-08-24 11:22:22 +1000

    tidied up register page design

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { Box, Paper, TextField, Button, Link } from '@mui/material'
+import { Box, Paper, TextField, Button, Link, Typography } from '@mui/material'
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { register } from '../actions/auth';
@@ -6,15 +6,16 @@ import { Navigate, Link as RouterLink, useNavigate } from 'react-router-dom';
 
 export default function Register() {
 
-    const [ name, setName ] = useState('');
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const [ passwordConfirmation, setPasswordConfirmation ] = useState('');
-    const [ phoneNo, setPhoneNo ] = useState('');
-    const [ address, setAddress ] = useState('');
-    const [ successful, setSuccessful ] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState('');
+    const [phoneNo, setPhoneNo] = useState('');
+    const [address, setAddress] = useState('');
+    const [successful, setSuccessful] = useState(false);
     const { message } = useSelector(state => state.message)
     const { isLoggedIn } = useSelector((state) => state.auth);
+    const navigate = useNavigate();
 
     const onChangeName = (e) => {
         const name = e.target.value;
@@ -47,88 +48,102 @@ export default function Register() {
         dispatch(register(email, name, password, passwordConfirmation, phoneNo, address))
             .then(() => {
                 setSuccessful(true);
+                navigate('/')
             })
             .catch(() => {
                 setSuccessful(false);
             });
-        
-      };
+
+    };
     if (isLoggedIn) {
         return <Navigate to="/profile" replace={true} />;
     }
     return (
-        <Paper>
-            <p>Sign up!</p>
-            <Box component="form" onSubmit={handleRegister} noValidate sx={{ mt: 1 }}>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                    onChange={onChangeEmail}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="name"
-                    label="Name"
-                    name="name"
-                    autoComplete="name"
-                    onChange={onChangeName}
-                />
-                <TextField
-                    margin="normal"
-                    fullWidth
-                    id="address"
-                    label="Address"
-                    name="address"
-                    autoComplete="address"
-                    onChange={onChangeAddress}
-                />
-                <TextField
-                    margin="normal"
-                    fullWidth
-                    id="phone-no"
-                    label="Phone Number"
-                    name="phone-no"
-                    autoComplete="phone"
-                    onChange={onChangePhoneNo}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="set-password"
-                    onChange={onChangePassword}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password-confirmation"
-                    label="Confirm Password"
-                    type="password"
-                    id="password-confirmation"
-                    autoComplete="confirm-password"
-                    onChange={onChangePasswordConfirmation}
-                />
-                <Link component={RouterLink} to="/login" variant="body2">
-                    Already have an account? Sign In!
-                </Link>
-                <br />
-                <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2 }}>
-                    Register
-                </Button>
-            </Box>
-        </Paper>
+        <Box sx={{ width: ["auto", 500], mx: "auto" }}>
+            <Paper
+                elevation={3}
+                sx={{
+                    marginTop: "5vh",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    p: 3,
+                }}
+            >
+            <Typography component="h1" variant="h5">
+                Register
+            </Typography>
+                <Box component="form" onSubmit={handleRegister} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        onChange={onChangeEmail}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="name"
+                        label="Name"
+                        name="name"
+                        autoComplete="name"
+                        onChange={onChangeName}
+                    />
+                    <TextField
+                        margin="normal"
+                        fullWidth
+                        id="address"
+                        label="Address"
+                        name="address"
+                        autoComplete="address"
+                        onChange={onChangeAddress}
+                    />
+                    <TextField
+                        margin="normal"
+                        fullWidth
+                        id="phone-no"
+                        label="Phone Number"
+                        name="phone-no"
+                        autoComplete="phone"
+                        onChange={onChangePhoneNo}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="set-password"
+                        onChange={onChangePassword}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password-confirmation"
+                        label="Confirm Password"
+                        type="password"
+                        id="password-confirmation"
+                        autoComplete="confirm-password"
+                        onChange={onChangePasswordConfirmation}
+                    />
+                    <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2 }}>
+                        Register
+                    </Button>
+                    <br />
+                    <Link component={RouterLink} to="/login" variant="body2">
+                        Already have an account? Sign In!
+                    </Link>
+                </Box>
+            </Paper>
+        </Box>
     );
 }
